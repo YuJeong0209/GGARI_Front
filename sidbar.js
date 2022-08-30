83638924ba2821f16c28d9be21636ed48d23e860
Junhyung Cho <jun@novaflo.kr>
2022-08-30 20:55:01 +0900

    end calory page.

--- sidbar.js
+++ sidbar.js
@@ -9,15 +9,16 @@ var sidebar = {
     //페이지 번호 등록하기!!
     pageIds : [
         [],
-        ["26035", "26059", "26060", "26061"],
+        ["26951", "26950", "26059", "26060", "26061"],
         ["26044"],
         ["26045"],
+        ["26058"],
         ["26802", "26801", "26803", "26800"],
-        ["26062", "26058", "26066", "26065", "26067"],      
+        ["26922", "26926", "26065", "26067"],
     ],
 
     showSidebar : () => {
-        outer : for(var i=1; i<6; i++){
+        outer : for(var i=1; i<7; i++){
             inner : for(let ids in sidebar.pageIds[i]){
                 if(location.href.includes(sidebar.pageIds[i][ids])){
                     $('#sidebar'+i).removeClass('d-none');                    
@@ -25,7 +26,6 @@ var sidebar = {
                 }                
             }
         }
-
     }
 
 }
